b8f8c7b9685396b95aa6c0f35e89479239f85cbc
MaheswariKarthi <kmmaheswari24@gmail.com>
2023-05-08 11:48:04 +0530

    -Initial is done to save the file structure

--- service.js
+++ service.js
@@ -2,19 +2,19 @@ const express=require('express')
 const serviceRouter=express.Router()
 const listOfUsers=[
     {
-        'id':1,
+        
         'name':'webapplication'
     },
     {
-        'id':2,
+        
         'name':'cyber security'
     },
     {
-        'id':3,
+        
         'name':'operating system'
     },
     {
-        'id':4,
+        
         'name':'datascience'
     },
 ]
@@ -23,19 +23,20 @@ serviceRouter.get('/',(request,response)=>{
     response.send("This is list of users")
 })
 serviceRouter.get('/:id([0-9]{1})',(request,response)=>{
-    const pageId=Number(request.params.id)
+    response.send(request.user.name)
    
-    const userId=listOfUsers.find((user)=>user.id === pageId)
-    if(!userId){
-        response.send('page not found')
-    }
-    else{
-        response.json(userId.name)
-    }
-   // response.send(`This is list of users ${request.params.id}`)
 })
+serviceRouter.param('id',(request,response,next,id)=>{
+    request.user=listOfUsers[id-1]
+    console.log(id);
+    next()
+})
+
+
 
 serviceRouter.get('/',(request,response)=>{
     response.send("this is service page")
 })
 module.exports=serviceRouter
+
+
